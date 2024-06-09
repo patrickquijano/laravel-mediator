a949da067124277201c7a72e30205ec14c26b895
Patrick Quijano <6316829+patrickquijano@users.noreply.github.com>
2024-06-09 05:47:26 +0000

    added service provider abstract

---
+++ src/Abstracts/Providers/HandlersServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PatrickQuijano\LaravelCQRS\Abstracts\Providers;
+
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\ServiceProvider as AbstractServiceProvider;
+
+/**
+ * @codeCoverageIgnore
+ */
+abstract class HandlersServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array<string, string>
+     */
+    public array $handlers = [];
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Bus::map($this->handlers);
+    }
+}
