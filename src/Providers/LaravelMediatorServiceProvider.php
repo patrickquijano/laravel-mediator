fc9ef617147b5dce78a53bc4a1fba36894138188
Patrick Quijano <6316829+patrickquijano@users.noreply.github.com>
2024-09-04 03:10:19 +0000

    updated singletons and boot method

--- src/Providers/LaravelMediatorServiceProvider.php
+++ src/Providers/LaravelMediatorServiceProvider.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace LaravelMediator\Providers;
 
 use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider as AbstractServiceProvider;
-use LaravelMediator\Contracts\Services\Mediator\DiscoverEventsService;
+use LaravelMediator\Contracts\Services\Mediator\GetBusEventsService;
+use LaravelMediator\Contracts\Services\Mediator\GetEventsService;
 
 class LaravelMediatorServiceProvider extends AbstractServiceProvider
 {
-    public const CACHE_KEY = 'event-listeners-map';
-
     /**
      * All of the container singletons that should be registered.
      *
@@ -21,17 +19,14 @@ class LaravelMediatorServiceProvider extends AbstractServiceProvider
     public $singletons = [
         \LaravelMediator\Contracts\Buses\CommandBus::class => \LaravelMediator\Buses\CommandBus::class,
         \LaravelMediator\Contracts\Buses\QueryBus::class => \LaravelMediator\Buses\QueryBus::class,
-        \LaravelMediator\Contracts\Services\Mediator\DiscoverEventsService::class => \LaravelMediator\Services\Mediator\DiscoverEventsService::class,
-        \LaravelMediator\Contracts\Services\Mediator\GetClassFromFileService::class => \LaravelMediator\Services\Mediator\GetClassFromFileService::class,
-        \LaravelMediator\Contracts\Services\Mediator\GetEventForListenerService::class => \LaravelMediator\Services\Mediator\GetEventForListenerService::class,
+        \LaravelMediator\Contracts\Services\Mediator\GetBusEventsService::class => \LaravelMediator\Services\Mediator\GetBusEventsService::class,
+        \LaravelMediator\Contracts\Services\Mediator\GetEventsService::class => \LaravelMediator\Services\Mediator\GetEventsService::class,
     ];
 
     public function boot(): void
     {
-        $this->publishes([__DIR__.'/../../config/mediator.php' => config_path('mediator.php')], 'mediator-config');
-        $eventsMap = Cache::rememberForever(self::CACHE_KEY, function () {
-            return $this->app->make(DiscoverEventsService::class)->handle();
-        });
-        Bus::map($eventsMap);
+        $events = $this->app->make(GetEventsService::class)->handle();
+        $busEvents = $this->app->make(GetBusEventsService::class)->handle($events);
+        Bus::map($busEvents);
     }
 }
